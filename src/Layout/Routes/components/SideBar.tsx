0a48be6dbc6ae4ad00470fd3e5da718c4a7f87d6
NoOne451 <ranjithdurai111@gmail.com>
2024-06-30 16:51:09 +0530

    client side done

--- src/Layout/Routes/components/SideBar.tsx
+++ src/Layout/Routes/components/SideBar.tsx
@@ -33,12 +33,13 @@ const navLinks = [
 export function SideBar() {
   const dispatch: AppDispatch = useDispatch();
   return (
-    <div className="fixed  flex sm:right-[40px] sm:bottom-[30px] left-0 bottom-0 sm:p-0  w-dvw gap-5 justify-center p-6  sm:flex-col  z-[3]">
+    <div className="fixed  flex sm:right-[40px] sm:bottom-[30px] justify-center left-0 sm:left-auto bottom-0 sm:justify-start w-full sm:w-auto sm:flex-col gap-8 z-[3]">
       {navLinks.map((link, index) => {
         return (
           <NavLink
             to={link.route}
             className={({ isActive }) => (isActive ? 'active' : '')}
+            key={index}
           >
             <img
               className={`w-[40px] h-[40px]  rounded-full bg-cover bg-customGreen border  border-customGreen border-solid border-spacing-[50px] p-[5px] hover:scale-125 duration-300`}
